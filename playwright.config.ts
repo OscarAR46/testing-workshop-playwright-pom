f8fcf033e0ae764d71fc1f402f49d8c745325923
Vincent Ngobeh <vincentngobeh@gmail.com>
2026-07-02 13:15:17 +0100

    feat: remove microsoft edge

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,14 +68,14 @@ module.exports = defineConfig({
         trace: "off",
       },
     },
-    {
-      name: "MicrosoftEdge",
-      use: {
-        ...devices["Desktop Edge"], // MS Edge desktop
-        channel: "msedge",
-        screenshot: "only-on-failure",
-        trace: "off",
-      },
-    },
+    // {
+    //   name: "MicrosoftEdge",
+    //   use: {
+    //     ...devices["Desktop Edge"], // MS Edge desktop
+    //     channel: "msedge",
+    //     screenshot: "only-on-failure",
+    //     trace: "off",
+    //   },
+    // },
   ],
 });
